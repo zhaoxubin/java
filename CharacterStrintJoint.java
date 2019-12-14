6ac19de8d810937b05b6d5e39a208d42c1e34143
zhaoxubin <2581939960@qq.com>
2019-12-14 21:04:28 +0800

    字符串通过+连接的基本练习

---
+++ CharacterStrintJoint.java
@@ -0,0 +1,12 @@
+public class CharacterStrintJoint{
+     public static void main(String[] args){
+             String a="hello";
+             String b="world";
+             String c=a+b;
+             System.out.println(c);
+             String str="result =";
+              int d=10;
+              int e=20;
+              System.out.println(str+d+e) ;
+      }
+}
