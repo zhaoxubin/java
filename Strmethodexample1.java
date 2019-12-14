c1a98831a6ab92c2e9bfe4abe03af8a7ef6315d8
zhaoxubin <2581939960@qq.com>
2019-12-14 21:19:34 +0800

    String的使用中的方法，找到字符串中的对应位置的字符与输出相应字符的位置

---
+++ Strmethodexample1.java
@@ -0,0 +1,8 @@
+public class Strmethodexample1{
+    public static void main(String[ ] args){
+          String str="ABCDEF";
+          System.out.println(str.charAt(2));
+          System.out.println(str.indexOf('D')) ;
+     }
+
+}
