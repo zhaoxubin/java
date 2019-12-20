5ec0ec8102a190fd1b24475a2196bbf31ee4f1fc
zhaoxubin <2581939960@qq.com>
2019-12-20 23:22:13 +0800

    掌握java正确的输出方式

---
+++ OutputFormCompare.java
@@ -0,0 +1,8 @@
+public class Test{
+   public static void main(String[] args){
+      int a=10;
+      int b=20;
+     System.out.println("a+b"+"结果result="+a+b);//输出结果对比，掌握正确的输出方式
+     System.out.println("a+b"+"result="+(a+b));
+    }
+}
