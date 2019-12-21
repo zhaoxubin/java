b72261e9e40d77490c75e7a8ba1eaeca33deb373
zhaoxubin <2581939960@qq.com>
2019-12-21 19:59:06 +0800

    不用中间变量交换两个数的内容，此处用了两种办法，一是用加减法操作，二是用逻辑异或操作。

---
+++ ExchangeContent.java
@@ -0,0 +1,16 @@
+public class ExchangeContent{
+    public static void main(String[] args){
+      /* int  a=10;
+       int b=20;
+       a=a+b;
+       b=a-b;
+       a=a-b;
+*/
+     int a=10;
+     int b=20;
+     a=a^b;
+      b=a^b;
+      a=a^b;
+   System.out.println("a="+a+"  "+"b="+b); 
+      }
+}
