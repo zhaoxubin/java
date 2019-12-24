925913c3c3c6512da3cb1cb6a17263754bae344d
zhaoxubin <2581939960@qq.com>
2019-12-24 23:55:24 +0800

    计算1/1-1/2+1/3-1/4+1/5 …… + 1/99 - 1/100 的值

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add {
+    public static void main(String[] args) {
+        double i=1;
+        double sum=0;
+        for(;i<100;i++){
+            sum+=1/i;
+        }
+        System.out.println(sum);
+    }
+}
