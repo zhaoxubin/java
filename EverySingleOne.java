c6c3306a38fb598024bbae4ebd618a0dfc04a68f
zhaoxubin <2581939960@qq.com>
2019-12-27 09:10:56 +0800

    输出一个整数的每一位，采用&1运算.

---
+++ EverySingleOne.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class EverySingleOne {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int num=scanner.nextInt();
+        output(num);
+    }
+    public static void output(int num) {
+        if (num > 9) {
+            output(num/10);
+        }
+        System.out.println(num % 10);
+    }
+}
