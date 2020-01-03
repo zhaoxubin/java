e2f793cdb65daa7ebcee1428355845d7e908be25
zhaoxubin <2581939960@qq.com>
2020-01-03 22:30:08 +0800

    将数组转换为字符串（引用包Arrays，调用Array.toString方法）

---
+++ ArrayToString.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class ArrayToString {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        String newArray = Arrays.toString(array);
+        System.out.println("newArray «"+newArray);
+    }
+}
