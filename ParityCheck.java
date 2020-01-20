c0f2d17c3ddbe051b6edb32468a000314de7931c
zhaoxubin <2581939960@qq.com>
2020-01-20 21:57:40 +0800

    运用三元运算符判断一个数是否为偶数

---
+++ ParityCheck.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class ParityCheck {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入一个整数");
+        long number = scan.nextLong();
+        String check = (number % 2 == 0) ? "这个数是偶数" : "这个数是奇数";
+        System.out.println(check);
+    }
+}
