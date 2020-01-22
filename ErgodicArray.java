42b7cbc18799dfd905ee74d6226dd2b04efaabb8
zhaoxubin <2581939960@qq.com>
2020-01-22 22:25:47 +0800

    使用while循环与自增运算符实现循环遍历数组

---
+++ ErgodicArray.java
@@ -0,0 +1,11 @@
+
+public class ErgodicArray {
+    public static void main(String[] args) {
+        String[] arr = new String[]{"zhang","zhao","wu","zhou"};
+        int index = 0;
+        while(index < arr.length){
+            System.out.println(arr[index++]);
+        }
+    }
+}
+
