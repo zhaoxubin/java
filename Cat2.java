0d8a8a7b442abe425582c9a7466ba1f549b2f60b
zhaoxubin <55921289+zhaoxubin@users.noreply.github.com>
2020-02-02 20:13:00 +0800

    重新计算对象的哈希码
    一种简单的计算哈希码的方式是将重写equals()方法时使用到的成员变量，乘以不同的质数然后求和，以此作为新的哈希码

---
+++ Cat2.java
@@ -0,0 +1,46 @@
+public class Cat{
+    private String name;
+    private int age;
+    private double weight;
+    private Color color;
+    public Cat(String name,int age,double weight,Color color){
+         this.name = name;
+         this.age = age;
+         this.weight = weight;
+         this.color = color;
+    }
+    @Override
+    public boolean equals(Object obj){
+         if(this == obj){
+             return true;
+        }
+        if(obj == null){
+             return false;
+        }
+        if(getClass() != obj.getClass){
+             return false;
+        }
+        Cat cat = (Cat)obj;
+        return name.equals(cat.name)&&(age == cat.age)
+                &&(weight == cat.weight)&&(color.equals(cat.color));
+      }
+    @Overrid
+    public int hashCode(){
+     return 7*name.hashCode()+11*new Integer(age).hashCode()+13
+            *new Double(weight).hashCode()+17*color.hashCode();
+    }
+  }
+ public class Test{
+        public static void main(String[] args){
+        Cat cat1 = new Cat("java",12,21,Color.BLACK);
+        Cat cat2 = new Cat("c++",12,21,Color.WHITE);
+        Cat cat3 = new Cat("java",12,21,Color.BLACK);
+        System.out.println("猫咪1号的哈希码" + cat1.hashCode());
+        System.out.println("猫咪2号哈希码" + cat2.hashCode());
+        System.out.println("猫咪3号哈希码" + cat3.hashCode());
+        System.out.println("猫咪1号是否与猫咪2号相同" + cat1.equals(cat2));
+        System.out.println("猫咪1号是否与猫咪3号相同" + cat1.equals(cat3));
+     }
+   }
+    
+  
