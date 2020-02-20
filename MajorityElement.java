e9a2c3d1245083c397132cc52797eba7e4252ff6
zhaoxubin <2581939960@qq.com>
2020-02-20 21:46:58 +0800

    给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。

---
+++ MajorityElement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       int temp = 0;
+        for(int i = 0; i < nums.length ;i++) {
+            int count = 0;
+            for(int j = 0; j < nums.length ; j++){
+              if(nums[i] == nums[j]){
+                  count++;
+              }
+            }
+            if(count > (nums.length/2)){
+                temp = nums[i];
+            break;
+            }
+        }
+     return temp;
+    }
+}
