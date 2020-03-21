92337054ab45afefe4695277a031b1e900aa3791
zhaoxubin <2581939960@qq.com>
2020-03-21 20:25:05 +0800

    判断链表是否带环

---
+++ hasCycle.java
@@ -0,0 +1,12 @@
+public class hasCycle(ListNode head){
+    ListNode fast = head;
+    ListNode slow = head;
+    while(fast != null && fast.next != null ){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow == fast){
+            return true;
+        }
+        }
+        return false;
+}
